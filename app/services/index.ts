063060c69b0b0dc0c03bd7aace02851e307fa1a4
Karim248119 <karimoda66@gmail.com>
2025-01-10 05:17:00 +0200

    first commit

--- app/services/index.ts
+++ app/services/index.ts
@@ -1,20 +1,21 @@
+"use client";
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
 client.interceptors.request.use(
   (config) => {
-    const token = sessionStorage.getItem("token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+    if (typeof window !== "undefined") {
+      const token = window.sessionStorage.getItem("token");
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
     }
     return config;
   },
-  (error) => {
-    return Promise.reject(error);
-  }
+  (error) => Promise.reject(error)
 );
 
 export default client;
